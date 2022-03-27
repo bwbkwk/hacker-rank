d85e2a2b452701de7abcead62f156e02e88cd20b
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2022-03-27 12:26:21 +0900

    Update Two Strings.py

--- interview-preparation-kit/dictionaries and hashmaps/Two Strings.py
+++ interview-preparation-kit/dictionaries and hashmaps/Two Strings.py
@@ -4,7 +4,46 @@ import math
 import os
 import random
 import re
+import sysa#!/bin/python3
+
+import math
+import os
+import random
+import re
 import sys
+
+#
+# Complete the 'twoStrings' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+# x is substring of s1 and s2 iff the smaller substring y 
+# where y susbstring of x is also a susbtring of s1 and s2.
+# therefore we just need to find intersection of every alphabet of
+# s1 and s2
+def twoStrings(s1, s2):
+    return "YES" if len(set(s1).intersection(set(s2))) > 0 else "NO"
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
 
 #
 # Complete the 'twoStrings' function below.
