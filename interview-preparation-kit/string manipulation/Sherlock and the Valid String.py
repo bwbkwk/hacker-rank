0eff785f32aa02c60286e2be4c55c0e5490e609a
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2022-03-28 07:50:44 +0900

    Update Sherlock and the Valid String.py

--- interview-preparation-kit/string manipulation/Sherlock and the Valid String.py
+++ interview-preparation-kit/string manipulation/Sherlock and the Valid String.py
@@ -15,22 +15,31 @@ from collections import Counter
 # The function accepts STRING s as parameter.
 #
 
-# soon to be commented
+
 def isValid(s):
+    # count occurence for every alphabet appear in string s
     cs = Counter(s)
+    # count frequency of occurences
     freq = Counter([cs[k] for k in cs])
     
-    # print(freq)
+    # find the top 3 frequency
     most_common = freq.most_common(3)
-    print(most_common)
-    if len(most_common) > 2:
-        return "NO"
+    # print(most_common)
+    
+    # if only 1 alphabet appear return 'YES'
     if len(most_common) == 1:
         return "YES"
-    if most_common[1] == (1,1):
-        return "YES"
+    # if 2 alphabet appear check the other condition
+    if len(most_common) == 2:
+        # if we can delete second alphabet as it only appear 1 return "YES"
+        if most_common[1] == (1,1):
+            return "YES"
+        # if delete one character from string make it valid
+        if abs(most_common[1][0] - most_common[0][0]) ==1 and most_common[1][1] == 1:
+            return "YES"
     
-    return "YES" if abs(most_common[1][0] - most_common[0][0]) ==1 and most_common[1][1] == 1 else "NO"
+    # otherwise return no
+    return "NO"
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
