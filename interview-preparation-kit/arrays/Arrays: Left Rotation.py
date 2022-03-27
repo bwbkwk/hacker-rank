73e96b9282c853a0046243c785fae5f9360c1ec0
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2022-03-27 12:52:59 +0900

    Update Arrays: Left Rotation.py

--- interview-preparation-kit/arrays/Arrays: Left Rotation.py
+++ interview-preparation-kit/arrays/Arrays: Left Rotation.py
@@ -16,6 +16,11 @@ import sys
 #
 
 def rotLeft(a, d):
+    # use array slicing to get the right part and append it
+    # with the left part. e.g. for a = 'abcdefg' and d = 5
+    #  0123456
+    # 'abcdefg'
+    # a[5:] = 'fg' and a[:5] = 'abcde', so  a[d:] + a[:d] = 'fgabcde'
     return a[d:] + a[:d]
     
 if __name__ == '__main__':
