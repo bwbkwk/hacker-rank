5c5913066cc5fe745dca651f9552768f3f088dc1
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2022-03-27 12:22:53 +0900

    Update Sherlock and Anagrams.py

--- interview-preparation-kit/dictionaries and hashmaps/Sherlock and Anagrams.py
+++ interview-preparation-kit/dictionaries and hashmaps/Sherlock and Anagrams.py
@@ -16,8 +16,11 @@ def generateSubstringDict(s):
     len_s = len(s)
     S = [s[i:j] for i in range(len_s) for j in range(i+1,len_s+1)]
     
-    # make Counter to frozenset make it can be used as dictionary key
+    # Counter of string contains all possible character 'k' with its 
+    # frequency 'v'. make Counter to frozenset make it can be used 
+    # as other dictionary/ Counter key
     dictArr = [frozenset(Counter(s).items()) for s in S]
+    
     return dictArr
 
 # solution 1 (TLE)
@@ -45,20 +48,24 @@ def sherlockAndAnagrams(s):
     
     # print(substrs)
     
-    map = {}
+    # create a Counter to count occurences of every possible anagram
+    cnt = Counter()
     
     for substr in substrs:
-        map[substr] = map.get(substr,0) + 1
+        cnt[substr] += 1
     
     anagram_count = 0
     
-    for k in map:
+    # for every possible value of anagram, dont need to worry of value of 1
+    # since the formula will make the resulting number of 0
+    for k in cnt:
         # formula all possible combination from map[k] count
         # example, for value of 4 (1,2,3,4)
         # the combination is given by (1,2), (1,3), (1,4) = 3
         # plus (2,3), (2,4) = 2 and plus (3,4) = 1
         # 3 + 2 + 1 = 4 * 3 /2
-        anagram_count += (map[k] * (map[k]-1))//2
+        anagram_count += (cnt[k] * (cnt[k]-1))//2
+        
     return anagram_count
     
 if __name__ == '__main__':
