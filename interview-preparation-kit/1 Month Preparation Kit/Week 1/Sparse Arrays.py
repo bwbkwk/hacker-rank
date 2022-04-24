9da583bf0ffbecd59dd63b52948c3628e365aa04
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2022-04-24 11:33:15 +0900

    Create Sparse Arrays.py

---
+++ interview-preparation-kit/1 Month Preparation Kit/Week 1/Sparse Arrays.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    # count every occurences of unique string using Counter datatype
+    cnt = Counter(strings)
+    
+    # return the occurence of each query q.
+    return [cnt[q] for q in queries]
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
