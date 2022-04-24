26f1ff79fdf3d9e4aaa8cb496922f8f1055a459d
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2022-04-24 11:29:20 +0900

    Update Time Conversion.py

--- interview-preparation-kit/1 Month Preparation Kit/Week 1/Time Conversion.py
+++ interview-preparation-kit/1 Month Preparation Kit/Week 1/Time Conversion.py
@@ -14,12 +14,16 @@ import sys
 #
 
 def timeConversion(s):
-    
+    # get the hour
     newHour = int(s[:2])
+    # get the last 2 character and check whether is AM or PM
     if s[-2:] == 'PM':
+        # if newHour not equal to 12 add 12 hour so it follows
+        # 24-hour system
         if newHour != 12:
             newHour = (newHour +12)
     else:
+        # mod by 12 so if the hour is 12 it become 00.
         newHour %= 12
     return "{:02d}:{}".format(newHour,s[3:-2])
     
