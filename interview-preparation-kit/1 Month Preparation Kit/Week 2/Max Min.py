65fe2d310ceee5167cf25b1b2d091c213e582b1c
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2022-04-25 15:46:21 +0900

    Create Max Min.py

---
+++ interview-preparation-kit/1 Month Preparation Kit/Week 2/Max Min.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr): 
+    arr.sort()
+    mn = sys.maxsize
+    # fit k for array index that start from zero
+    k-=1
+    for i in range(len(arr)-k):
+        mn = min(mn, arr[i+k] - arr[i])
+    return mn
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
