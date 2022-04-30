27c16ec0c07498f67078d527fd1e33372d7eb881
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2022-04-30 23:45:31 +0900

    Create Reverse a doubly linked list.py

---
+++ interview-preparation-kit/1 Month Preparation Kit/Week 3/Reverse a doubly linked list.py
@@ -0,0 +1,68 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'reverse' function below.
+#
+# The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+# The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+
+def reverse(llist):
+    while True:
+        temp = llist.next
+        llist.next, llist.prev = llist.prev, llist.next
+        if temp == None:
+            return llist
+        llist = temp
+        
+        
+
+if __name__ == '__main__':
