3cfa0b3766353dabaa0a8c1a2612e274de75258e
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2022-10-12 23:25:36 +0900

    Create sequence equation.py
    difficulty: easy

---
+++ problem solving/sequence equation.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'libraryFine' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d1
+#  2. INTEGER m1
+#  3. INTEGER y1
+#  4. INTEGER d2
+#  5. INTEGER m2
+#  6. INTEGER y2
+#
+
+def libraryFine(d1, m1, y1, d2, m2, y2):
+    if y1 > y2:
+        return 10000
+    if y1 < y2:
+        return 0
+    if m1 > m2:
+        return (m1 - m2) * 500
+    if m1 < m2:
+        return 0
+    return max((d1 - d2),0) * 15
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    d1 = int(first_multiple_input[0])
+
+    m1 = int(first_multiple_input[1])
+
+    y1 = int(first_multiple_input[2])
+
+    second_multiple_input = input().rstrip().split()
+
+    d2 = int(second_multiple_input[0])
+
+    m2 = int(second_multiple_input[1])
+
+    y2 = int(second_multiple_input[2])
+
+    result = libraryFine(d1, m1, y1, d2, m2, y2)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
