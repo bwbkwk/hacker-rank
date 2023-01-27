2b0bfc91f1fbe304b3eb25ddcd94279efc8dfaba
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2023-01-27 16:03:46 +0900

    Create sansa and xor.py
    difficulty: medium, 
    note: 
    1.  n xor n equal to zero and 
    2. occurence of n in continuous subsequence that contain n must be even when the sequence has even number of elements

---
+++ problem solving/sansa and xor.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sansaXor' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def sansaXor(arr):
+    if len(arr) % 2 == 0:
+        return 0
+    
+    res = 0
+    for i in range(0,len(arr),2):
+        res ^= arr[i]
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = sansaXor(arr)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
