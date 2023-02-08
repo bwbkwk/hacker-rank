3eafd53bc44d48303185439a8cca6b1770f3f9d1
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2023-02-08 17:22:11 +0900

    Create camel case.py

---
+++ problem solving/camel case.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    test = [1 if ord(c) >= 65 and ord(c) <= 90 else 0 for c in s]
+    return sum(test)+1
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
