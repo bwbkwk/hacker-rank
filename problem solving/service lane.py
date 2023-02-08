9df1585159766f9cedf074c32784bb3f01a812fe
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2023-02-08 16:51:21 +0900

    Create service lane.py

---
+++ problem solving/service lane.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'serviceLane' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY cases
+#
+
+def serviceLane(width, cases):
+    min_widths = [min(set(width[case[0]:case[1]+1])) for case in cases]
+    return min_widths
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(width, cases)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
