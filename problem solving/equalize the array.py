8a2e508544d71548f2ee16fdf394cf2423cc427a
Julio C. Young <43823941+bwbkwk@users.noreply.github.com>
2023-10-18 14:06:41 +0900

    Create equalize the array.py
    Difficulty: easy

---
+++ problem solving/equalize the array.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    cnt = Counter(arr)
+    return sum([c for _,c in cnt.most_common()[1:]])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
